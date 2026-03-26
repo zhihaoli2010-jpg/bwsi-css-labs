87d3c48d95b2e523dbe55f748a34bf085bc42310
zhihaoli2010-jpg <zhihaoli2010@gmail.com>
2026-03-26 14:48:01 -0400

    change the robot speed from 5 to 8 in the header comment

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,6 +3,9 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+
+
+    robot_speed = 3 # m/s
 """
 
 def main():
